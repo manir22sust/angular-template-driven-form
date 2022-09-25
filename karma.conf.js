6687de321972f59400c6c3a775fdfe738510a58b
Md. Manir Uddin <manir22sust@gmail.com>
2022-09-25 15:27:42 +0200

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-template-driven-form'),
+      dir: require('path').join(__dirname, './coverage/angular-reactive-forms'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-template-driven-form';
+  title = 'angular-reactive-forms';
+
+  constructor() {}
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+  }
+
+  getValue(f: FormControl) {
+    console.log(f);
+  }
 }
